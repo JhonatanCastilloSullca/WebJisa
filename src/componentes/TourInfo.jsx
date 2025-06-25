b8365295726f4f03ef654ec9adbbc1e96e82b55a
David Miranda <dmirandatarco@gmail.com>
2025-06-25 17:18:24 -0500

    por el momento

--- src/componentes/TourInfo.jsx
+++ src/componentes/TourInfo.jsx
@@ -14,7 +14,7 @@ const TourInfo = ({ duracion, grupo, tipo, recojo }) => {
         <div className="w-full max-w-7xl mx-auto md:-mt-10 z-30 relative md:px-0 px-6 shadow-md bg-white rounded-lg">
             <div className="container mx-auto py-4">
                 <div className="flex mx-auto justify-center">
-                    <IconCategoria icon={ClockIcon} title={t('tours_detail.duracion')} subtitle={duracion == 1 ? 'full day' : subtitle+' day'} className='px-16 text-JisaCyan' />
+                    <IconCategoria icon={ClockIcon} title={t('tours_detail.duracion')} subtitle={duracion == 1 ? 'full day' : duracion+' day'} className='px-16 text-JisaCyan' />
                     <SeparatorBar />
                     <IconCategoria icon={GroupUsers} title={t('tours_detail.max_grupo')} subtitle={`${grupo} paxs`} className='px-16 text-JisaCyan' />
                     <SeparatorBar />
