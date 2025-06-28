1d406ca26d196df5b3d0a8599d6f00a55e42b9dd
Jhonatan <jcastillosullca@gmail.com>
2025-06-28 11:54:09 -0500

    11542806

--- src/componentes/secciones/MenuInter.jsx
+++ src/componentes/secciones/MenuInter.jsx
@@ -2,7 +2,8 @@ import { NavLink } from "react-router-dom";
 import MenuHeader from "../MenuHeader";
 import CartIcon from "../../assets/icons/CartIcon";
 
-function MenuIntern({ menuOpen, menu = [] }) {
+function MenuIntern({ dataGeneral, menuOpen, menu = [] }) {
+  const header = dataGeneral?.header || [];
   return (
     <div
       className={`flex flex-col md:flex-row md:bg-white bg-white text-JisaCyan md:text-JisaCyan w-full transition-all duration-300 ease-in-out ${menuOpen ? "block" : "hidden md:flex"
@@ -10,19 +11,25 @@ function MenuIntern({ menuOpen, menu = [] }) {
     >
       <div className="w-full max-w-7xl mx-auto">
         <nav className="text-gray-900">
-          <div className="container mx-auto flex md:flex-row flex-col md:justify-center justify-start">
+          <div className="container mx-auto flex md:flex-row flex-col md:justify-center justify-start uppercase">
             <NavLink
               to="/"
-              className="relative block py-2 px-4 text-sm lg:text-base font-bold md:text-JisaCyan text-JisaCyan hover:text-JisaGris"
+              className="relative block py-2 px-4 text-sm lg:text-base font-bold md:text-JisaCyan text-black hover:text-JisaGris uppercase"
             >
               Inicio
             </NavLink>
             <MenuHeader menu={menu} />
             <NavLink
               to="/nosotros"
-              className="relative block py-2 px-4 text-sm lg:text-base font-bold md:text-JisaCyan text-JisaCyan hover:text-JisaGris"
+              className="relative block py-2 px-4 text-sm lg:text-base font-bold md:text-JisaCyan text-black hover:text-JisaGris uppercase"
             >
               Sobre Nosotros
+            </NavLink>
+            <NavLink
+              to={header.enlace}
+              className="bg-JisaCyan text-white rounded-xl text-center flex px-6 md:font-medium font-bold md:text-base text-xl py-1 items-center"
+            >
+              {header.boton_accion}
             </NavLink>
             <NavLink
               to="/carrito"
@@ -30,7 +37,7 @@ function MenuIntern({ menuOpen, menu = [] }) {
             >
               <div className="absolute w-10">
                 <CartIcon size={16} />
-                <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-JisaCyan bg-red-500 border-2 border-white rounded-full -top-2 -end-2">
+                <div className="absolute right-2  inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-4 -end-2">
                   20
                 </div>
               </div>
