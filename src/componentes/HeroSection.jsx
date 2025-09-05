320577c32688dbc2f9410aa45f15d7605b081f69
David Miranda Tarco <dmirandatarco@gmail.com>
2025-09-05 13:04:14 -0500

    quitando boton de slider

--- src/componentes/HeroSection.jsx
+++ src/componentes/HeroSection.jsx
@@ -22,14 +22,14 @@ const HeroSection = ({
                     <div className="md:col-span-3 col-span-5 flex flex-col md:items-start items-center">
                         <h1 className="md:text-7xl text-5xl md:w-full  md:text-left text-center font-bold mb-4">{title}</h1>
                         <p className="text-xl mb-6 w-full md:text-left text-center md:pe-72">{description}</p>
-                        {buttonText && buttonLink && (
+                        {/* {buttonText && buttonLink && (
                             <a
                                 href={buttonLink}
                                 className="px-16 py-2 bg-JisaCyan text-white font-bold text-lg rounded-lg shadow-md hover:bg-gray-200 hover:text-JisaGris transition"
                             >
                                 {buttonText}
                             </a>
-                        )}
+                        )} */}
                     </div>
                 </div>
             </div>
