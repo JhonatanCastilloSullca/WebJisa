a465ac78c6a3b8d8d45af55ca1971af2f097963d
David Miranda <dmirandatarco@gmail.com>
2025-10-15 17:31:56 -0500

    web

--- src/componentes/HeroSectionMidle.jsx
+++ src/componentes/HeroSectionMidle.jsx
@@ -10,14 +10,14 @@ const HeroSectionMidle = ({
 }) => {
     return (
         <div
-            className="md:h-[100vh] h-auto bg-cover bg-center relative flex flex-col justify-end items-center text-white text-center p-4 md:py-32 py-16"
+            className="md:h-[95vh] h-auto bg-cover bg-center relative flex flex-col justify-end items-center text-white text-center p-4 md:py-32 py-16"
             style={{ backgroundImage: `url(${encodeURI(backgroundImage)})` }}
         >
             <div
                 className="absolute inset-0"
                 style={{ backgroundColor: overlayColor }}
             ></div>
-            <div className="md:absolute relative bottom-0 max-w-7xl w-full flex flex-col">
+            <div className=" relative bottom-0 max-w-7xl w-full flex flex-col">
                 <div className="grid grid-cols-5 gap-4">
                     <div className="md:col-span-5 col-span-5 flex flex-col md:items-center items-center">
                         <h1 className="md:text-5xl text-2xl md:w-full text-white  md:text-center text-center font-bold mb-4">{title}</h1>
